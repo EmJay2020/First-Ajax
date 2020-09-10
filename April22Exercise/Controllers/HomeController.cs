c3907e1f2292354bd1ca9fd0d4ce792ca3a8cd93
Menachem Jacobs <mj.ch770@gmail.com>
2020-09-10 15:16:02 -0400

    Add project files.

---
+++ April22Exercise/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using April22Exercise.Models;
+
+namespace April22Exercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Reverser(string x)
+        {
+            string result = "";
+            for(int i = x.Length-1; i >= 0; i--)
+            {
+                result += x[i];
+            }
+            return Json(result);
+        }
+
+    }
+}
---
+++ April22Exercise/wwwroot/js/Ajax.js
@@ -0,0 +1,9 @@
+﻿$(() => {
+
+    $("#my-btn").on('click', function () {
+        let reversed = $("#textbox").val();
+        $.get(`/home/reverser?x=${reversed}`, function (obj) {
+            $("#my-h1").text(obj);
+        });
+    })
+})
